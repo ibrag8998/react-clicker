b6c90742b6fd93ed0746b327accb4fc68125a838
ibrag8998 <ibragdzh@gmail.com>
2022-05-07 16:26:08 +0300

    some styles

--- src/helpers/HelperList.jsx
+++ src/helpers/HelperList.jsx
@@ -5,12 +5,17 @@ import HelperItem from "./HelperItem"
 export default function HelperList(props) {
   return (
     <div className="helper-list">
-      {helpers.map(item => (
-        <HelperItem
-          item={item}
-          key={item.id}
-          onClick={() => props.onHelperItemClick(item.cps, item.price)}
-        />
+      {helpers.map((item, i) => (
+        <div key={item.id}>
+          <HelperItem
+            item={item}
+            onClick={() => props.onHelperItemClick(item.cps, item.price)}
+          />
+          <div
+            className="helper-item-divider"
+            style={i === helpers.length - 1 ? { display: "none" } : null}
+          ></div>
+        </div>
       ))}
     </div>
   )
